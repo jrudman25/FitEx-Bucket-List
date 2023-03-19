035872f860662934e9fb9bb788fdfdbc7aabe35d
Ross Shaffer <rossgshaffer@vt.edu>
2023-03-19 11:49:31 -0400

    Added Global Scavenger and Bucket Lists

---
+++ src/ScavengerListGlobal.js
@@ -0,0 +1,45 @@
+const scavengerList = [
+    {
+        objective: "Take a picture of the drillfield at sunset.",
+        points: 2
+    },
+    {
+        objective: "Take a picture of yourself with ducks at the Duckpond",
+        points: 2
+    },
+    {
+        objective: "Take a picture of yourself at the Frank Beamer Statue",
+        points: 2
+    },
+    {
+        objective: "Take a picture of yourself sitting on the edge at the Pylons",
+        points: 2
+    },
+    {
+        objective: "Take a picture of yourself with the Hokiebird Statue by D2",
+        points: 2
+    },
+    {
+        objective: "Take a picture of yourself at the Farmer's Market",
+        points: 2
+    },
+    {
+        objective: "Take a picture of yourself at the Torgerson Bridge",
+        points: 2
+    },
+    {
+        objective: "Take a picture of yourself in front of Burruss",
+        points: 2
+    },
+    {
+        objective: "Take a picture of yourself inside of Hahn Horticulture Garden",
+        points: 2
+    },
+    {
+        objective: "Take a picture of yourself with William Addison Caldwell",
+        points: 2
+    }
+
+];
+
+export default scavengerList;
