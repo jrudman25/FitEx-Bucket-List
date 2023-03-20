bff31cc7a343cf876bdb36c81aa06773e978f88e
nairks01 <nairks01@vt.edu>
2023-03-20 19:54:33 -0400

    initial skeleton for bucket list pages

--- src/BucketList.js
+++ src/BucketList.js
@@ -1,9 +1,138 @@
-import React from "react";
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import DoneAllRoundedIcon from '@mui/icons-material/DoneAllRounded';
+import FormatListBulletedRoundedIcon from '@mui/icons-material/FormatListBulletedRounded';
+import HikingRoundedIcon from '@mui/icons-material/HikingRounded';
+import CameraEnhanceRoundedIcon from '@mui/icons-material/CameraEnhanceRounded';
+import Box from '@mui/material/Box';
+import {Button, Card, CardActions, CardContent, CardMedia, Typography} from "@mui/material";
 
-const BucketList = () => {
+//Used from: https://mui.com/material-ui/react-bottom-navigation/
+export default function BucketList() {
+    const [value, setValue] = React.useState('all');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+
     return (
-        <h1>Bucket List</h1>
-    );
-};
+        <React.Fragment>
+            <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
+                <BottomNavigationAction
+                    label="All"
+                    value="all"
+                    icon={<FormatListBulletedRoundedIcon />}
+                />
+                <BottomNavigationAction
+                    label="Hunt"
+                    value="hunt"
+                    icon={<CameraEnhanceRoundedIcon />}
+                />
+                <BottomNavigationAction
+                    label="Hikes"
+                    value="hikes"
+                    icon={<HikingRoundedIcon />}
+                />
+                <BottomNavigationAction
+                    label="Completed"
+                    value="completed"
+                    icon={<DoneAllRoundedIcon />}
+                />
+            </BottomNavigation>
 
-export default BucketList;
+            {value === 'all' && (
+                <Card sx={{ maxWidth: 499 }}>
+                    <CardMedia
+                        component="img"
+                        alt="hike"
+                        height="140"
+                        image=""
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Hike 1
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description for hike 1
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Start</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+
+
+
+            )}
+            {value === 'hunt' && (
+                <Card sx={{ maxWidth: 499 }}>
+                    <CardMedia
+                        component="img"
+                        alt="hike"
+                        height="140"
+                        image=""
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Hunt 1
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description for hunt 1
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Start</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+            )}
+            {value === 'hikes' && (
+                <Card sx={{ maxWidth: 499 }}>
+                    <CardMedia
+                        component="img"
+                        alt="hike"
+                        height="140"
+                        image=""
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Hike 1
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description for hike 1
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Start</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+            )}
+            {value === 'completed' && (
+                <Card sx={{ maxWidth: 499 }}>
+                    <CardMedia
+                        component="img"
+                        alt="hike"
+                        height="140"
+                        image=""
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Hike 1
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description for hike 1
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Start</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+            )}
+        </React.Fragment>
+    );
+}
