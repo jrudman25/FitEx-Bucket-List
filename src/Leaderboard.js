3a4c98f8665d157bed28c6baf77a7138f0c6d1a3
Kevin Yang <keviny@vt.edu>
2023-03-22 13:51:26 -0400

    basic leaderboard

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import './Leaderboard.css';
 
-const Leaderboard = () => {
+const leaderboardData = [
+    { name: 'Player 1', score: 100 },
+    { name: 'Player 2', score: 200 },
+    { name: 'Player 3', score: 150 },
+    { name: 'Player 4', score: 50 },
+];
+
+function Leaderboard() {
     return (
-        <h1>Leaderboard</h1>
+        <div className="leaderboard-container">
+            <h2>Leaderboard</h2>
+            <table className="leaderboard-table">
+                <thead>
+                <tr>
+                    <th>Rank</th>
+                    <th>Name</th>
+                    <th>Points</th>
+                </tr>
+                </thead>
+                <tbody>
+                {leaderboardData.map(({ name, score }, index) => (
+                    <tr className={index % 2 === 0 ? 'even-row' : 'odd-row'} key={index}>
+                        <td>{index + 1}</td>
+                        <td>{name}</td>
+                        <td>{score.toLocaleString()}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
-};
+}
 
 export default Leaderboard;
