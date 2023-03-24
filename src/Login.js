d02ced53a4916fc298b2b8f3d31847ff10b4a367
Jordan Rudman <71191637+jrudman25@users.noreply.github.com>
2023-03-24 05:54:08 -0400

    Basic login screen

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Box, TextField, Button } from '@mui/material';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+
+    const handleUsernameChange = event => setUsername(event.target.value);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        // Use username to authenticate the user
+        // Redirect to main screen if login is successful
+        // Display error message if login is unsuccessful
+        alert("login attempted with username: " + username)
+    };
+
     return (
-        <h1>Login</h1>
+        <div>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    minHeight: '100vh'
+                }}
+            >
+                <h1>Sign in</h1>
+                <form onSubmit={handleSubmit}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                        <TextField
+                            id="username"
+                            label="Username"
+                            type="text"
+                            value={username}
+                            onChange={handleUsernameChange}
+                        />
+                        <Button type="submit" variant="contained" sx={{ marginTop: '1rem' }}>
+                            Login
+                        </Button>
+                    </Box>
+                </form>
+            </Box>
+        </div>
     );
 };
 
