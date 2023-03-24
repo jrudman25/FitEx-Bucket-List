b3903de3372ab7720bb0f319e7129c998e79bf58
Jordan Rudman <71191637+jrudman25@users.noreply.github.com>
2023-03-24 02:28:22 -0400

    Fixed wrong image and broken logo

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,5 @@
 import { Nav } from './NavBarElement';
+import bucketListIcon from './img/bucketListIcon.png'
 
 const NavBar = () => {
     return (
@@ -6,7 +7,7 @@ const NavBar = () => {
             <Nav style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
                     <div id={"imageContainer"} style={{ height: '50px', marginRight: '10px' }}>
-                        <img src={"bucketListIcon.png"} style={{ width: '100%', height: '100%' }} />
+                        <img src={bucketListIcon} style={{ width: '100%', height: '100%' }} />
                     </div>
                     <div style={{ color: 'white', fontSize: '24px' }}>Hokie Bucket List</div>
                 </div>
