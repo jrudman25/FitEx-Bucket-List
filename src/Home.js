c7847ddb412f2c5a5e6f7ddc68f26282561b5c1f
Ross Shaffer <rossgshaffer@vt.edu>
2023-04-07 17:56:17 -0400

    small edit

--- src/Home.js
+++ src/Home.js
@@ -123,75 +123,70 @@ const Home = () => {
 };
 
 export default Home;
-// import React, { useState, useEffect } from "react";
-// import { Typography } from '@mui/material';
-// import defaultUser from './img/defaultUser.jpg';
-// import { useLocation, Navigate } from 'react-router-dom';
-// import './Home.css';
+// <Typography variant="h5" sx={{ mt: 3 }} textAlign="center">
+//     Recommended Hikes
+// </Typography>
 //
-// const Home = () => {
-//
-//     const location = useLocation();
-//
-//     const [email, setEmail] = useState(sessionStorage.getItem('username') || '');
-//     useEffect(() => {
-//         if (location.state && location.state.username) {
-//             sessionStorage.setItem('username', location.state.username);
-//             setEmail(location.state.username);
-//         }
-//     }, [location.state]);
-//
-//     const [image, setImage] = useState(getUserImage(email) || defaultUser);
-//     useEffect(() => {
-//         const userImages = JSON.parse(localStorage.getItem('userImages') || '{}');
-//         setImage(userImages[email] || defaultUser);
-//     }, [email]);
-//
-//     const handleImageUpload = (event) => {
-//         const reader = new FileReader();
-//         reader.onload = (e) => {
-//             const base64Image = e.target.result;
-//             const userImages = JSON.parse(localStorage.getItem('userImages') || '{}');
-//             userImages[email] = base64Image;
-//             localStorage.setItem('userImages', JSON.stringify(userImages));
-//             setImage(base64Image);
-//         };
-//         reader.readAsDataURL(event.target.files[0]);
-//     }
-//
-//     function getUserImage(username) {
-//         const userImages = JSON.parse(localStorage.getItem('userImages') || '{}');
-//         return userImages[username];
-//     }
-//
-//     if (!(sessionStorage.getItem('isLoggedIn') === 'true')) {
-//         return <Navigate to="/" />;
-//     }
-//
-//     return (
-//         <div>
-//             <div className="image-upload-container">
-//                 <Typography variant="h4" sx={{ marginTop: '1rem', marginBottom: '1rem' }}>
-//                     Welcome{email ? `, ${email}` : ''}!
-//                 </Typography>
-//                 <label htmlFor="image-upload" className="image-upload-label">
-//                     <input
-//                         id="image-upload"
-//                         type="file"
-//                         accept="image/*"
-//                         onChange={handleImageUpload}
-//                         style={{ display: "none" }}
-//                     />
-//                     <img src={image} alt="Profile" className="profile-image" />
-//                 </label>
-//             </div>
-//             <div className="link-container">
-//                 <a href="/group" className="home-link">Group</a>
-//                 <a href="/leaderboard" className="home-link">Leaderboard</a>
-//                 <a href="/bucketlist" className="home-link">Bucket List</a>
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default Home;
+// {0 < totalVal <= 4 && easyHikes.map((item, index) => (
+//             <Card key={index}>
+//                 <CardMedia
+//                     component="img"
+//                     height="140"
+//                     image={item.image}
+//                     alt={item.name}
+//                 />
+//                 <CardContent>
+//                     <Typography variant="h5" component="h2">
+//                         {item.name}
+//                     </Typography>
+//                     <Typography color="textSecondary">
+//                         Points: {item.points}
+//                     </Typography>
+//                     <Typography color="textSecondary">
+//                         Distance: {item.length_distance} miles
+//                     </Typography>
+//                 </CardContent>
+//             </Card>
+//         ))}
+// {4 < totalVal <= 9 && moderateHikes.map((item, index) => (
+//             <Card key={index}>
+//                 <CardMedia
+//                     component="img"
+//                     height="140"
+//                     image={item.image}
+//                     alt={item.name}
+//                 />
+//                 <CardContent>
+//                     <Typography variant="h5" component="h2">
+//                         {item.name}
+//                     </Typography>
+//                     <Typography color="textSecondary">
+//                         Points: {item.points}
+//                     </Typography>
+//                     <Typography color="textSecondary">
+//                         Distance: {item.length_distance} miles
+//                     </Typography>
+//                 </CardContent>
+//             </Card>
+//         ))}
+// {9 < totalVal <= 12 && hardHikes.map((item, index) => (
+//             <Card key={index}>
+//                 <CardMedia
+//                     component="img"
+//                     height="140"
+//                     image={item.image}
+//                     alt={item.name}
+//                 />
+//                 <CardContent>
+//                     <Typography variant="h5" component="h2">
+//                         {item.name}
+//                     </Typography>
+//                     <Typography color="textSecondary">
+//                         Points: {item.points}
+//                     </Typography>
+//                     <Typography color="textSecondary">
+//                         Distance: {item.length_distance} miles
+//                     </Typography>
+//                 </CardContent>
+//             </Card>
+//         ))}
